7d1afb013f88b6bb6d121e94f21124eb23658502
Moca <36897041+MocaCDev@users.noreply.github.com>
2023-03-14 21:44:04 -0500

    Add files via upload

--- boot_protocol/protocol/b8000_api.h
+++ boot_protocol/protocol/b8000_api.h
@@ -5,8 +5,8 @@
 extern "C" {
 #endif
 
-#define cols        25
-#define rows        80
+#define rows        25
+#define cols        80
 
 /* Declare this in a header file somewhere. */
 static uint16 *vid_mem = (uint16 *)0xB8000;
@@ -31,7 +31,9 @@ enum color_attr
     yellow          = 0x0E,
     white           = 0x0F,
     wwhite          = 0xF0,
-    rred            = 0xF4
+    rred            = 0xF4,
+    cyan_white      = 0x3F,
+    cyan_red        = 0x34,
 };
 
 typedef struct cursor_info
@@ -42,10 +44,105 @@ typedef struct cursor_info
 
 static _cursor_info c_info = {.pos_x = 0, .pos_y = 0};
 
+/*
+ *  __inp: back-end function
+ *
+ *  Read-in a value from a port
+ *
+ *  Input: uint16 port
+ *  Output: uint8 rv(return-value from the port)
+ *  On Error: This function does not error
+ */
+uint8 __inp(uint16 port) {
+    uint8 rv;
+    
+    __asm__ __volatile__("in %0, %1": "=a"(rv): "dN"(port));
+    return rv;
+}
+/*
+ *  __outp: back-end function
+ *
+ *  Write a value to a port
+ *
+ *  Input: uint16 port, uint8 data
+ *  Output: None
+ *  On Error: This function does not error
+ */
+void __outp(uint16 port, uint8 data) {
+    __asm__ __volatile__("outb %0, %1":: "dN"(port), "a"(data));
+}
+
+#define reg_screen_ctr  0x3D4
+#define reg_screen_data 0x3D5
+
+/* Last cursor X position after enter is pressed. */
+static uint16 last_cursor_x = 0;
+
+void update_cursor_pos()
+{
+    uint16 pos = (c_info.pos_y * 80) + c_info.pos_x;
+    __outp(reg_screen_ctr, 14);
+    __outp(reg_screen_data, pos >> 8);
+    __outp(reg_screen_ctr, 15);
+    __outp(reg_screen_data, pos & 0xFF);
+}
+
+void init_cursor_pos()
+{
+    /* Set `pos_x`/`pos_y` to zero to initialize. */
+    c_info.pos_x = c_info.pos_y = 0;
+
+    update_cursor_pos();
+}
+
+void put_char(uint8 character)
+{
+    switch(character)
+    {
+        case '\n':
+        {
+            c_info.pos_y++;
+            last_cursor_x = c_info.pos_x;
+            c_info.pos_x = 0;
+            goto end;
+        }
+        case '\t':
+        {
+            c_info.pos_x+=4;
+            goto end;
+        }
+        case 0x0E:
+        {
+            if(c_info.pos_x >= 1)
+            {
+                c_info.pos_x--;
+                vid_mem[c_info.pos_x + (c_info.pos_y * 80)] = (cyan_white << 8) | (0xFF & ' ');
+                goto end;
+            }
+
+            if(c_info.pos_x == 0 && c_info.pos_y > 0)
+            {
+                c_info.pos_y--;
+                c_info.pos_x = last_cursor_x;
+                goto end;
+            }
+            return;
+        }
+        /* Do nothing if the above does not run. */
+        default: break;
+    }
+
+    vid_mem[c_info.pos_x + (c_info.pos_y * 80)] =  (cyan_white << 8) | (0xFF & character);
+    c_info.pos_x++;
+
+    end:
+    update_cursor_pos();
+}
+
 void print(const uint8 *str)
 {
     lsize i = 0;
-    uint8 color = white;
+    uint8 color = cyan_white;
 
     while(str[i] != '\0')
     {
@@ -91,9 +188,9 @@ void print(const uint8 *str)
                         if(str[i+1] == 'e')
                         {
                             i++;
-                            color = white;
+                            color = cyan_white;
                         }
-                        else color = red;
+                        else color = cyan_red;
                         i++;
                         goto redo;
                     }
@@ -143,12 +240,29 @@ void print(const uint8 *str)
     return;
 }
 
+uint16 *memset(uint16 *dest, uint16 val, lsize count){
+	uint16* dstPtr = (uint16 *)dest;
+
+    for (; count != 0; count--) *dstPtr++ = val;
+	
+	return dest;
+}
+
+uint16 get_text_attribute(uint8 foreground, uint8 background)
+{
+    return (foreground << 4) | (background & 0x0F);
+}
+
+uint16 get_text_value(uint8 character, uint8 foreground, uint8 background){
+	return character | (get_text_attribute(foreground, background) << 8);
+}
+
 void clear_screen(uint8 color)
 {
     uint16 i = 0;
-    while(i < (rows * cols))
+    while(i < rows*cols)
     {
-        vid_mem[i] = (color << 8) | (0xFF & ' ');
+        memset((uint16 *)0xB8000 + (i * cols), get_text_value(' ', cyan, white), cols);
         i++;
     }
     //vid_mem = (uint16 *) 0xB8000;
